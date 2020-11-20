0b5ca166f178058539fced61a89eff7c9625ab92
Michael Kozlov <red.bymike@gmail.com>
2020-11-20 15:23:55 +0300

    add files

---
+++ script.js
@@ -0,0 +1,68 @@
+const text = document.querySelector("#textarea");
+const href = document.querySelector("#texthref");
+const textTag = document.querySelector("#text-name-tag");
+const urlTag = document.querySelector("#text-url-tag");
+
+let b = document.querySelector(".b");
+let b1 = document.querySelector(".b1");
+let result = document.querySelector(".output");
+let result2 = document.querySelector(".output-tags xmp");
+
+b.addEventListener("click", ()=> {
+    let arrText = text.value.split('\n');
+    let arrHref = href.value.split('\n');
+    arrHrefNew = arrHref.map(str => {
+        if(str.indexOf("http://xn--1-----8vefgeugb1aimdekb0alg1ambq6dd41a2a.xn--p1ai") !== -1){
+           return str.replace('http://xn--1-----8vefgeugb1aimdekb0alg1ambq6dd41a2a.xn--p1ai', '');
+        }
+        if(str.indexOf("https://xn--1-----8vefgeugb1aimdekb0alg1ambq6dd41a2a.xn--p1ai" !== -1)){
+           return str.replace('https://xn--1-----8vefgeugb1aimdekb0alg1ambq6dd41a2a.xn--p1ai', '');
+        }
+        if(str.indexOf("http://xn--1-----8vefgeugb1aimdekb0alg1ambq6dd41a2a.xn--p1ai/shop/folder") !== -1){
+            return str.replace('http://xn--1-----8vefgeugb1aimdekb0alg1ambq6dd41a2a.xn--p1ai/shop/folder', '');
+         }
+        if(str.indexOf("https://xn--1-----8vefgeugb1aimdekb0alg1ambq6dd41a2a.xn--p1ai/shop/folder" !== -1)){
+            return str.replace('https://xn--1-----8vefgeugb1aimdekb0alg1ambq6dd41a2a.xn--p1ai/shop/folder', '');
+         }
+    });
+    let resArr = [];
+    for(let i = 0; i < arrText.length; i++){
+        if(i === arrText.length - 1){
+            resArr.push(`<li style="clear: both; list-style: none; padding: 0 10px 10px 0;"><a alt="${arrText[i]}" href="${arrHrefNew[i]}" title="${arrText[i]}">еще подборки</a></li>`);
+        } else {
+        resArr.push(`<li style="float: left; list-style: none; padding: 0 10px 10px 0;"><a alt="${arrText[i]}" href="${arrHrefNew[i]}" title="${arrText[i]}">${arrText[i].split("").map((l, i) => i === 0 ? l.toUpperCase() : l.toLowerCase()).join("")}</a></li>`);
+        }
+    }
+    for(let i = 0; i < resArr.length; i++){
+            result.innerHTML += `<xmp>${resArr[i]}</xmp>`;
+        }
+});
+
+b1.addEventListener("click", ()=> {
+    let arrTextTag = textTag.value.split('\n');
+    let arrHrefTag = urlTag.value.split('\n');
+    arrHrefTagNew = arrHrefTag.map(str => {
+        if(str.indexOf("http://xn--1-----8vefgeugb1aimdekb0alg1ambq6dd41a2a.xn--p1ai") !== -1){
+           return str.replace('http://xn--1-----8vefgeugb1aimdekb0alg1ambq6dd41a2a.xn--p1ai', '');
+        }
+        if(str.indexOf("https://xn--1-----8vefgeugb1aimdekb0alg1ambq6dd41a2a.xn--p1ai" !== -1)){
+           return str.replace('https://xn--1-----8vefgeugb1aimdekb0alg1ambq6dd41a2a.xn--p1ai', '');
+        }
+        if(str.indexOf("http://xn--1-----8vefgeugb1aimdekb0alg1ambq6dd41a2a.xn--p1ai/shop/folder") !== -1){
+            return str.replace('http://xn--1-----8vefgeugb1aimdekb0alg1ambq6dd41a2a.xn--p1ai/shop/folder', '');
+         }
+        if(str.indexOf("https://xn--1-----8vefgeugb1aimdekb0alg1ambq6dd41a2a.xn--p1ai/shop/folder/" !== -1)){
+            return str.replace('https://xn--1-----8vefgeugb1aimdekb0alg1ambq6dd41a2a.xn--p1ai/shop/folder', '');
+         }
+    });
+    let resArrTag = [];
+    for(let i = 0; i < arrTextTag.length; i++){
+        if(arrTextTag[i].length !== 0){
+            resArrTag.push(`<a href="${arrHrefTagNew[i]}" target="_blank">${arrTextTag[i]}</a><br />`.trim());
+        }
+    }
+    for(let i = 0; i < resArrTag.length; i++){
+        result2.innerHTML += `${resArrTag[i]}\n`;
+        }
+});
+
