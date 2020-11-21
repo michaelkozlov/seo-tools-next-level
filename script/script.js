cfb77ea20e08b58ef191fcb12fbae302321081ce
Michael Kozlov <red.bymike@gmail.com>
2020-11-21 17:20:58 +0300

    refactor script.js add new Function

--- script/script.js
+++ script/script.js
@@ -15,7 +15,66 @@ b2.addEventListener("click", () => {
     result.innerHTML = "";
     let arrText = text.value.trim().split('\n');
     let arrHref = href.value.trim().split('\n');
-    arrHrefNew = arrHref.map(str => {
+    checkb2(arrText, arrHref);
+});
+
+function checkb2(arrText, arrHref) {
+    let checkArrText = arrText.some(str => str.indexOf("все подборки") !== -1);
+    let checkArrHref = arrHref.some(str => str.indexOf("tags") !== -1);
+    if (!checkArrText) {
+        text.classList.add("check-no");
+    } else {
+        text.classList.remove("check-no");
+        text.classList.add("check-yes");
+    }
+    if (!checkArrHref) {
+        href.classList.add("check-no");
+    } else {
+        href.classList.remove("check-no");
+        href.classList.add("check-yes");
+    }
+    if (checkArrText && checkArrHref) {
+        text.classList.remove("check-no");
+        href.classList.remove("check-no");
+        text.classList.add("check-yes");
+        href.classList.add("check-yes");
+        generateCodeTagInCategories(arrText, arrHref)
+    }
+}
+
+function generateCodeTagInCategories(arrText, arrHref) {
+    let arrHrefNew = cut(arrHref);
+    let resArr = [];
+    for (let i = 0; i < arrText.length; i++) {
+        if (i === arrText.length - 1) {
+            resArr.push(`<li style="clear: both; list-style: none; padding: 0 10px 10px 0;"><a alt="${arrText[i]}" href="${arrHrefNew[i]}" title="${arrText[i]}">еще подборки</a></li>`);
+        } else {
+            resArr.push(`<li style="float: left; list-style: none; padding: 0 10px 10px 0;"><a alt="${arrText[i]}" href="${arrHrefNew[i]}" title="${arrText[i]}">${arrText[i].split("").map((l, i) => i === 0 ? l.toUpperCase() : l.toLowerCase()).join("")}</a></li>`);
+        }
+    }
+    for (let i = 0; i < resArr.length; i++) {
+        result.innerHTML += `<xmp>${resArr[i]}</xmp>`;
+    }
+}
+
+b1.addEventListener("click", () => {
+    result2.innerHTML = "";
+    let arrTextTag = textTag.value.split('\n');
+    let arrHrefTag = urlTag.value.split('\n');
+    let arrHrefTagNew = cut(arrHrefTag);
+    let resArrTag = [];
+    for (let i = 0; i < arrTextTag.length; i++) {
+        if (arrTextTag[i].length !== 0) {
+            resArrTag.push(`<a href="${arrHrefTagNew[i]}" target="_blank">${arrTextTag[i]}</a><br />`.trim());
+        }
+    }
+    for (let i = 0; i < resArrTag.length; i++) {
+        result2.innerHTML += `${resArrTag[i]}\n`;
+    }
+});
+
+function cut(arr){
+    return arr.map(str => {
         if (str.indexOf("http://xn--1-----8vefgeugb1aimdekb0alg1ambq6dd41a2a.xn--p1ai") !== -1) {
             return str.replace('http://xn--1-----8vefgeugb1aimdekb0alg1ambq6dd41a2a.xn--p1ai', '');
         }
@@ -29,61 +88,21 @@ b2.addEventListener("click", () => {
             return str.replace('https://xn--1-----8vefgeugb1aimdekb0alg1ambq6dd41a2a.xn--p1ai/shop/folder', '');
         }
     });
-    let resArr = [];
-    for (let i = 0; i < arrText.length; i++) {
-        if (i === arrText.length - 1) {
-            resArr.push(`<li style="clear: both; list-style: none; padding: 0 10px 10px 0;"><a alt="${arrText[i]}" href="${arrHrefNew[i]}" title="${arrText[i]}">еще подборки</a></li>`);
-        } else {
-            resArr.push(`<li style="float: left; list-style: none; padding: 0 10px 10px 0;"><a alt="${arrText[i]}" href="${arrHrefNew[i]}" title="${arrText[i]}">${arrText[i].split("").map((l, i) => i === 0 ? l.toUpperCase() : l.toLowerCase()).join("")}</a></li>`);
-        }
-    }
-    for (let i = 0; i < resArr.length; i++) {
-        result.innerHTML += `<xmp>${resArr[i]}</xmp>`;
-    }
-});
+}
 
-b1.addEventListener("click", () => {
-    result2.innerHTML = "";
-    let arrTextTag = textTag.value.split('\n');
-    let arrHrefTag = urlTag.value.split('\n');
-    arrHrefTagNew = arrHrefTag.map(str => {
-        if (str.indexOf("http://xn--1-----8vefgeugb1aimdekb0alg1ambq6dd41a2a.xn--p1ai") !== -1) {
-            return str.replace('http://xn--1-----8vefgeugb1aimdekb0alg1ambq6dd41a2a.xn--p1ai', '');
-        }
-        if (str.indexOf("https://xn--1-----8vefgeugb1aimdekb0alg1ambq6dd41a2a.xn--p1ai" !== -1)) {
-            return str.replace('https://xn--1-----8vefgeugb1aimdekb0alg1ambq6dd41a2a.xn--p1ai', '');
-        }
-        if (str.indexOf("http://xn--1-----8vefgeugb1aimdekb0alg1ambq6dd41a2a.xn--p1ai/shop/folder") !== -1) {
-            return str.replace('http://xn--1-----8vefgeugb1aimdekb0alg1ambq6dd41a2a.xn--p1ai/shop/folder', '');
-        }
-        if (str.indexOf("https://xn--1-----8vefgeugb1aimdekb0alg1ambq6dd41a2a.xn--p1ai/shop/folder/" !== -1)) {
-            return str.replace('https://xn--1-----8vefgeugb1aimdekb0alg1ambq6dd41a2a.xn--p1ai/shop/folder', '');
-        }
-    });
-    let resArrTag = [];
-    for (let i = 0; i < arrTextTag.length; i++) {
-        if (arrTextTag[i].length !== 0) {
-            resArrTag.push(`<a href="${arrHrefTagNew[i]}" target="_blank">${arrTextTag[i]}</a><br />`.trim());
-        }
-    }
-    for (let i = 0; i < resArrTag.length; i++) {
-        result2.innerHTML += `${resArrTag[i]}\n`;
-    }
-});
-
-buttonClearOne.addEventListener("click", ()=>{
+buttonClearOne.addEventListener("click", () => {
     clearBlock("one");
 });
 
-buttonClearTwo.addEventListener("click", ()=>{
+buttonClearTwo.addEventListener("click", () => {
     clearBlock("two");
 });
 
-function clearBlock(numberBlock){
-    if(numberBlock === "one"){
+function clearBlock(numberBlock) {
+    if (numberBlock === "one") {
         result2.innerHTML = "";
     }
-    if(numberBlock === "two"){
+    if (numberBlock === "two") {
         result.innerHTML = "";
     }
 }
